8e64f6082fff02ca207eb898d5d748a3dd5a5a71
Matheus Gonçales <matheusgoncales05@gmail.com>
2024-03-04 12:03:47 -0300

    Reduzindo o constructor

--- src/App.js
+++ src/App.js
@@ -3,38 +3,41 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
-
-constructor (props) {
-  super(props);
-  this.handlePClick = this.handlePClick.bind(this);
-
-  this.state = {
-    name: 'Matheus Gonçales'
+  state = {
+    name: 'Matheus Gonçales',
+    counter: 0
   };
+
+
+handlePClick = () => {
+  this.setState({name: 'Henrique'});
 }
 
-handlePClick() {
-  const {name} = this.state;
-  console.log(`clicado ${name}`);
+handleAClick = (event) => {
+  event.preventDefault();
+  const {counter} = this.state;
+
+  this.setState({counter: counter + 1});
 }
 
   render() {
-    const name = this.state.name
+    const {name, counter} = this.state
     
     return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p onClick={this.handlePClick}>
-          {name}
+          {name} {counter}
         </p>
         <a
+          onClick={this.handleAClick}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Link
         </a>
       </header>
     </div>
