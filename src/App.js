b876c95e90c36b802e34bd40539b080270ad5c0e
Matheus Gonçales <matheusgoncales05@gmail.com>
2024-03-04 11:45:37 -0300

    criação de componentes de classe com estado (stateful)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
+class App extends Component {
+
+constructor (props) {
+  super(props);
+  this.handlePClick = this.handlePClick.bind(this);
+
+  this.state = {
+    name: 'Matheus Gonçales'
+  };
+}
+
+handlePClick() {
+  const {name} = this.state;
+  console.log(`clicado ${name}`);
+}
+
+  render() {
+    const name = this.state.name
+    
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p onClick={this.handlePClick}>
+          {name}
         </p>
         <a
           className="App-link"
@@ -19,7 +38,29 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    ); 
 }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
