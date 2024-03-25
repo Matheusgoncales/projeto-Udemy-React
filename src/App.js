2e8559d3e28a94317b062f0cef40871aaa2e0939
Matheus Gonçales <matheusgoncales05@gmail.com>
2024-03-25 11:28:51 -0300

    handletimeout, e componentes update, unmount e did mount

--- src/App.js
+++ src/App.js
@@ -4,45 +4,64 @@ import { Component } from 'react';
 
 class App extends Component {
   state = {
-    name: 'Matheus Gonçales',
-    counter: 0
+    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: 'O titulo 1',
+        body: 'O corpo 1'
+      },
+      {
+        id: 2,
+        title: 'O titulo 2',
+        body: 'O corpo 2'
+      },
+      {
+        id: 3,
+        title: 'O titulo 3',
+        body: 'O corpo 3'
+      }
+    ]
   };
 
+  timeoutUpdate = null;
 
-handlePClick = () => {
-  this.setState({name: 'Henrique'});
-}
+  componentDidMount() {
+   this.handleTimeout();
+  }
 
-handleAClick = (event) => {
-  event.preventDefault();
-  const {counter} = this.state;
+  componentWillUnmount(){
+    clearTimeout(this.timeoutUpdate);
+  }
+  
+  componentDidUpdate() {
+    this.handleTimeout();
+  }
 
-  this.setState({counter: counter + 1});
-}
+  handleTimeout = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = 'O titulo mudou';
+
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({posts, counter: counter + 1})
+    }, 2000)
+  }
 
   render() {
-    const {name, counter} = this.state
-    
+    const { posts, counter } = this.state
+
     return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={this.handlePClick}>
-          {name} {counter}
-        </p>
-        <a
-          onClick={this.handleAClick}
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Link
-        </a>
-      </header>
-    </div>
-    ); 
-}
+      <div className="App">
+        <h1>{counter}</h1>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1 >{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 // function App() {
