0a1c7eb9f38f17793124be030932fb21480fcad9
Matheus Gonçales <matheusgoncales05@gmail.com>
2024-03-26 11:38:33 -0300

    Request api imagens e população dos container

--- src/App.js
+++ src/App.js
@@ -16,10 +16,18 @@ class App extends Component {
   loadPosts = async () => {
     const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
 
-    const [posts] = await Promise.all([postsResponse]);
-    const postsJson = await posts.json();
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
 
-    this.setState({ posts: postsJson })
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+    const postsJson = await posts.json();
+    const photoJson = await photos.json();
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return {...post, cover: photoJson[index].url}
+    })
+
+    this.setState({ posts: postsAndPhotos })
   }
 
   render() {
@@ -29,9 +37,12 @@ class App extends Component {
       <section className='container'>
         <div className='posts'>
           {posts.map(post => (
-            <div key={post.id} className='postContent'>
-              <h1 >{post.title}</h1>
-              <p>{post.body}</p>
+            <div className='post'>
+              <img src={post.cover} alt={post.title}/>
+              <div key={post.id} className='postContent'>
+                <h1 >{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
             </div>
           ))}
         </div>
